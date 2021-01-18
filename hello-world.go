dcd4516ef2e6196ed551fbfd8b9255438332db17
james-borwick <james.borwick@icloud.com>
2021-01-18 14:19:04 +0000

    Changed some stuff

--- hello-world.go
+++ hello-world.go
@@ -1,17 +1,19 @@
 package main
 
 import (
-        "fmt"
-        "net/http"
-        "log"
+    "fmt"
+    "net/http"
+    "log"
+    "os"
 )
 
 func main() {
-        http.HandleFunc("/", handler)
-        port := "8080"
-        log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
+    http.HandleFunc("/", handler)
+    port := "8080"
+    log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-        fmt.Fprintf(w, "Hello World!\n")
+    hostname, _ := os.Hostname()
+    fmt.Fprintf(w, "Hostname: %s\nDeployment: violet\n", hostname)
 }
